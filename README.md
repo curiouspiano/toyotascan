97ee2674e4b57209bc7231f9b669cd10ef75c579
curiouspiano <16311692+curiouspiano@users.noreply.github.com>
2022-05-09 18:49:12 -0400

    A new start

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toyotascan
+## Usage
+use `python3` to execute `get.py`
+
+![image](https://user-images.githubusercontent.com/16311692/167506805-9f3f65ab-306d-4d71-b7ce-a2b7375f622f.png)
+
+dealer information can be found https://www.toyota.com/dealers/dealer/{DEALERID}/ where you replace `{DEALERID}` with the dealer found in script output. (i.e. https://www.toyota.com/dealers/dealer/32137/ 
+
+## Modifying per region
---
+++ get.py
@@ -0,0 +1,74 @@
+import requests
+import json
+
+ENDPOINT = "https://www.toyota.com/inventory/search"
+
+BODY = {"brand":"TOY","facetfields":[],"fields":[],"group":"false","groupfield":"","groupmode":"full","mode":"content","pagesize":"70","pagestart":"0","relevancy":"false","sortfield":"MSRP","sortorder":"ASC","show":{"accessory":{"derived":"true","zeroDollar":"false","extraCostColor":"false","exclude":"false"}},"filter":{"year":["2022"],"series":["86"],"model":[],"grade":[],"enginetransmission":[],"drive":[],"bed":[],"cab":[],"exteriorcolor":[],"interiorcolor":[],"accessory":[],"packages":[],"andfields":["accessory","packages"],"dealers":["32137"],"region":["500"]}}
+
+HEADERS = {}
+
+def get_data(endpoint):
+    r = requests.post(endpoint, json=BODY)
+    if(r.status_code == 200):
+        return r.json()
+    else:
+        print(r.status_code)
+
+
+def get_dealer_info(deal_id):
+    addr = "https://www.toyota.com/dealers/dealer/0"
+    new_addr = addr + str(deal_id)
+    r = requests.get(new_addr)
+    if(r.status_code == 200):
+        print(r.text)
+    else:
+        print(r.status_code)
+
+
+def read_file(file_name):
+    f = open(file_name)
+    data = json.load(f)
+    f.close()
+    return data
+
+def write_vins(raw):
+    data = raw['body']['response']
+    current = read_vins()
+    file1 = open('vins.txt', 'a')
+    new = []
+    for key in data['docs']:
+        if key['vin'] not in current:
+            new.append(key['vin'])
+
+    for key in new:
+        file1.write(key)
+        file1.write('\n')
+
+    file1.close()
+
+def read_vins():
+    file1 = open('vins.txt', 'r')
+    data = file1.read().split('\n')
+    return data
+
+def parse_data(data):
+    print('VIN - availability - msrp - trans - dealerId')
+    resp = data['body']['response']
+    count = 1
+    pre_vins = read_vins()
+    for key in resp['docs']:
+        if key['vin'] not in pre_vins:
+            print("*{0}. {1} - {2} - {3} - {4} - {5}".format(count, key['vin'], key['availabilityDate'], key['msrp'], key['model']['transmission'], key['dealerFields']['dealer']))
+        else:
+            print("{0}. {1} - {2} - {3} - {4} - {5}".format(count, key['vin'], key['availabilityDate'], key['msrp'], key['model']['transmission'], key['dealerFields']['dealer']))
+        count += 1
+
+    write_vins(data)
+
+if __name__ == "__main__":
+    #data = read_file('out.json')
+    data = get_data(ENDPOINT)
+
+    parse_data(data)
+
+
